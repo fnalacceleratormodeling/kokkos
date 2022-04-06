501004792798c0dc911810f2688e6c248a6a6775
Sajid Ali <sajidsyed2021@u.northwestern.edu>
2022-04-06 12:43:50 -0500

    incorporate qiming fixes for AppleClang/libomp
    	modified:   cmake/kokkos_corner_cases.cmake
    	modified:   cmake/kokkos_enable_devices.cmake

--- cmake/kokkos_corner_cases.cmake
+++ cmake/kokkos_corner_cases.cmake
@@ -33,11 +33,11 @@ IF(KOKKOS_CXX_COMPILER_ID STREQUAL AppleClang AND KOKKOS_ENABLE_OPENMP)
   #I have to hackily pretend that compiler flags are compiler definitions
   #and that linker flags are libraries
   #also - this is easier to use than CMakeCheckCXXSourceCompiles
+  FIND_PACKAGE(OpenMP REQUIRED)
   TRY_COMPILE(APPLECLANG_HAS_OMP
     ${KOKKOS_TOP_BUILD_DIR}/corner_cases
     ${KOKKOS_SOURCE_DIR}/cmake/compile_tests/clang_omp.cpp
-    COMPILE_DEFINITIONS -Xpreprocessor -fopenmp
-    LINK_LIBRARIES -lomp
+    LINK_LIBRARIES OpenMP::OpenMP_CXX
   )
   IF (NOT APPLECLANG_HAS_OMP)
     UNSET(APPLECLANG_HAS_OMP CACHE) #make sure CMake always re-runs this
